d6472a885f77d4b2b30c02d7df37a7ab8367d4fa
廖恒泰 hengtai.liao <aiqing0342@qq.com>
2018-01-05 11:39:45 +0800

    first commit

---
+++ js/java.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function(){
+	$('*[href]').each(function(){
+		var href=encodeURI($(this).attr('href'));
+		$(this).attr('href',href);
+	});
+	$('*[src]').each(function(){
+		var src=encodeURI($(this).attr('src'));
+		$(this).attr('src',src);
+	});
+}); 
